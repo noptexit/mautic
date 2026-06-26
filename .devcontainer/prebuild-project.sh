7ad2446af6cb4f749cfaa5d4357f1830d57e559a
fedys <miroslav.fedeles@gmail.com>
2026-06-26 12:47:05 +0200

    Open .md files in preview by default

--- .devcontainer/prebuild-project.sh
+++ .devcontainer/prebuild-project.sh
@@ -5,3 +5,4 @@ curl -fsSL https://ddev.com/install.sh | bash
 ddev start -y --skip-hooks
 ddev composer install --no-interaction
 ddev poweroff
+mkdir -p .vscode && echo '{"workbench.editorAssociations": {"*.md": "vscode.markdown.preview.editor"}}' > .vscode/settings.json
