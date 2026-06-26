bc01da7334ca00697e602a7f21c972ad9414e306
fedys <miroslav.fedeles@gmail.com>
2026-06-26 13:41:52 +0200

    Prevent modification of CODESPACES-READY.md in git

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -2,6 +2,7 @@
 set -e
 
 shownFile="/tmp/.codespaces-welcome-shown"
+readyTplFile=".devcontainer/CODESPACES-READY.tpl.md"
 readyFile=".devcontainer/CODESPACES-READY.md"
 mauticUrl="https://${CODESPACE_NAME}-8443.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 phpMyAdminUrl="https://${CODESPACE_NAME}-8036.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
@@ -10,6 +11,6 @@ mailpitUrl="https://${CODESPACE_NAME}-8027.${GITHUB_CODESPACES_PORT_FORWARDING_D
 
 if [[ "${CODESPACES:-}" = "true" ]] && [[ ! -f "$shownFile" ]]; then
     touch "$shownFile"
-    sed -i "s|{mauticUrl}|${mauticUrl}|g; s|{mailpitUrl}|${mailpitUrl}|g; s|{phpMyAdminUrl}|${phpMyAdminUrl}|g" "$readyFile"
+    sed "s|{mauticUrl}|${mauticUrl}|g; s|{mailpitUrl}|${mailpitUrl}|g; s|{phpMyAdminUrl}|${phpMyAdminUrl}|g" "$readyTplFile" > "$readyFile"
     code "$readyFile"
 fi
